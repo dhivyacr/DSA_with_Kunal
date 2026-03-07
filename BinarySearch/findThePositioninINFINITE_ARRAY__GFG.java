25cb407215a42ccc3c79597d7a92736851d188ad
Dhivya Dharshini A <145836214+dhivyacr@users.noreply.github.com>
2026-03-07 11:55:37 +0530

    Refactor InfiniteArray class and method names

--- BinarySearch/findThePositioninINFINITE_ARRAY__GFG.java
+++ BinarySearch/findThePositioninINFINITE_ARRAY__GFG.java
@@ -1,36 +1,41 @@
 import java.util.*;
-class Main{
-    public static void main(String[]args){
-        int [] arr = {3, 5, 7, 9, 10, 90,100, 130, 140, 160, 170};
-        int target=10;
-        System.out.println(finalans(arr,target));
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(ans(arr, target));
     }
-    
-    static int finalans(int [] arr, int target){
+    static int ans(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
         int start = 0;
         int end = 1;
-        
-        while(target>arr[end]){
-            int newstart = end+1;
-            end = end + (end - start + 1)*2;
-            start=newstart;
+
+        // condition for the target to lie in the range
+        while (target > arr[end]) {
+            int temp = end + 1; // this is my new start
+            // double the box value
+            // end = previous end + sizeofbox*2
+            end = end + (end - start + 1) * 2;
+            start = temp;
         }
-        
-        return binarySearch(arr,target,start,end);
+        return binarySearch(arr, target, start, end);
+
     }
-    
-    static int binarySearch(int [] arr, int target,int start,int end){
-        
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(target==arr[mid]){
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
                 return mid;
-            }
-            else if(target>=arr[mid]){
-                start=mid+1;
-            }
-            else{
-                 end=mid-1;
             }
         }
         return -1;
